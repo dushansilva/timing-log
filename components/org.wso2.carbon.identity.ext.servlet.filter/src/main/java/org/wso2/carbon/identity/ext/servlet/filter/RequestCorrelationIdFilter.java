2a8716c3c341a6922de6d74283f27e655e5f24fb
dushansilva <dushansilva95@gmail.com>
2018-09-12 15:34:51 +0530

    filter support query param as a config

--- components/org.wso2.carbon.identity.ext.servlet.filter/src/main/java/org/wso2/carbon/identity/ext/servlet/filter/RequestCorrelationIdFilter.java
+++ components/org.wso2.carbon.identity.ext.servlet.filter/src/main/java/org/wso2/carbon/identity/ext/servlet/filter/RequestCorrelationIdFilter.java
@@ -24,8 +24,7 @@ import org.slf4j.MDC;
 import org.wso2.carbon.base.ServerConfiguration;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -41,15 +40,25 @@ import javax.servlet.http.HttpServletRequest;
 public class RequestCorrelationIdFilter implements Filter {
 
     private static final String HEADER_TO_CORRELATION_ID_MAPPING = "HeaderToCorrelationIdMapping";
+    private static final String QUERY_TO_CORRELATION_ID_MAPPING = "QueryToCorrelationIdMapping";
     private static final String CORRELATION_ID_MDC = "Correlation-ID";
     private Map<String, String> headerToIdMapping;
+    private Map<String, String> queryToIdMapping;
     private String correlationIdMdc = CORRELATION_ID_MDC;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+
         if (filterConfig.getInitParameter(HEADER_TO_CORRELATION_ID_MAPPING) != null) {
             Gson gson = new Gson();
             headerToIdMapping = gson.fromJson(filterConfig.getInitParameter(HEADER_TO_CORRELATION_ID_MAPPING),
+                    Map.class);
+
+        }
+
+        if (filterConfig.getInitParameter(QUERY_TO_CORRELATION_ID_MAPPING) != null) {
+            Gson gson = new Gson();
+            queryToIdMapping = gson.fromJson(filterConfig.getInitParameter(QUERY_TO_CORRELATION_ID_MAPPING),
                     Map.class);
 
         }
@@ -84,19 +93,65 @@ public class RequestCorrelationIdFilter implements Filter {
         }
     }
 
+    private void associateQueryParamsToThread(ServletRequest servletRequest) {
+
+        if (queryToIdMapping != null && (servletRequest instanceof HttpServletRequest)) {
+            HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+            for (Map.Entry<String, String> entry : queryToIdMapping.entrySet()) {
+                String queryConfigured = entry.getKey();
+                String correlationIdName = entry.getValue();
+                if (StringUtils.isNotEmpty(queryConfigured) && StringUtils.isNotEmpty(correlationIdName)) {
+                    Enumeration<String> parameterNames = httpServletRequest.getParameterNames();
+                    while (parameterNames.hasMoreElements()) {
+                        String queryReceived = parameterNames.nextElement();
+                        setQueryCorrelationIdValue(queryReceived, queryConfigured, httpServletRequest, correlationIdName);
+                    }
+                }
+            }
+        }
+    }
+
     private void associateHeadersToThread(ServletRequest servletRequest) {
 
         if (headerToIdMapping != null && (servletRequest instanceof HttpServletRequest)) {
             HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
             for (Map.Entry<String, String> entry : headerToIdMapping.entrySet()) {
-                String header = entry.getKey();
+                String headerConfigured = entry.getKey();
                 String correlationIdName = entry.getValue();
-                if (StringUtils.isNotEmpty(header) && StringUtils.isNotEmpty(correlationIdName)) {
-                    String headerValue = httpServletRequest.getHeader(header);
-                    if (StringUtils.isNotEmpty(headerValue)) {
-                        MDC.put(correlationIdName, headerValue);
+                if (StringUtils.isNotEmpty(headerConfigured) && StringUtils.isNotEmpty(correlationIdName)) {
+                    Enumeration<String> headerNames = httpServletRequest.getHeaderNames();
+                    while (headerNames.hasMoreElements()) {
+                        String headerReceived = headerNames.nextElement();
+                        setHeaderCorrelationIdValue(headerReceived, headerConfigured, httpServletRequest, correlationIdName);
                     }
                 }
+            }
+            if (MDC.get(correlationIdMdc) == null) {
+                associateQueryParamsToThread(servletRequest);
+            }
+        }
+    }
+
+    private void setHeaderCorrelationIdValue(String headerReceived, String headerConfigured,
+                                             HttpServletRequest httpServletRequest, String correlationIdName) {
+
+        if (StringUtils.isNotEmpty(headerReceived) && headerReceived.equalsIgnoreCase(headerConfigured)) {
+            String headerValue = httpServletRequest.getHeader(headerReceived);
+            if (StringUtils.isNotEmpty(headerValue)) {
+                MDC.put(correlationIdName, headerValue);
+            }
+        }
+    }
+
+    private void setQueryCorrelationIdValue(String queryReceived, String queryConfigured,
+                                            HttpServletRequest httpServletRequest, String correlationIdName) {
+
+        if (StringUtils.isNotEmpty(queryReceived) && queryReceived.equalsIgnoreCase(queryConfigured)) {
+            String queryValue = httpServletRequest.getParameter(queryReceived);
+            if (StringUtils.isNotEmpty(queryValue)) {
+                MDC.put(correlationIdName, queryValue);
             }
         }
     }
