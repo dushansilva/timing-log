af8bc10e7a578745459ae71bb8685ad86667191f
dushansilva <dushansilva95@gmail.com>
2018-09-19 15:11:11 +0530

    check if configuration is enabled from carbon.xml

--- components/org.wso2.carbon.identity.ext.pool.db.interceptor/src/main/java/org/wso2/carbon/identity/ext/jdbc/pool/interceptor/TimeLogInterceptor.java
+++ components/org.wso2.carbon.identity.ext.pool.db.interceptor/src/main/java/org/wso2/carbon/identity/ext/jdbc/pool/interceptor/TimeLogInterceptor.java
@@ -18,17 +18,16 @@
 
 package org.wso2.carbon.identity.ext.jdbc.pool.interceptor;
 
-import com.google.gson.Gson;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.tomcat.jdbc.pool.interceptor.AbstractQueryReport;
+import org.wso2.carbon.utils.CarbonUtils;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.sql.*;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 
@@ -58,33 +57,38 @@ public class TimeLogInterceptor extends AbstractQueryReport {
     @Override
     public Object createStatement(Object proxy, Method method, Object[] args, Object statement, long time) {
         try {
-            Object result = null;
-            String name = method.getName();
-            String sql = null;
-            Constructor<?> constructor = null;
-            if (this.compare("createStatement", name)) {
-                constructor = this.getConstructor(0, Statement.class);
-            } else if (this.compare("prepareStatement", name)) {
-                sql = (String) args[0];
-                constructor = this.getConstructor(1, PreparedStatement.class);
-                if (sql != null) {
-                    this.prepareStatement(sql, time);
-                }
+            if (CarbonUtils.getServerConfiguration().getFirstProperty("EnableTimingLogs")
+                    .equalsIgnoreCase("true")) {
+
+                return invokeProxy(method, args, statement, time);
             } else {
-                if (!this.compare("prepareCall", name)) {
-                    return statement;
-                }
-                sql = (String) args[0];
-                constructor = this.getConstructor(2, CallableStatement.class);
-                this.prepareCall(sql, time);
+                return statement;
             }
-            result = constructor.newInstance(new StatementProxy(statement, sql));
-            return result;
+
         } catch (Exception var11) {
-            timeLog.warn("Unable to create statement proxy for slow query report.", var11);
+            timeLog.warn("Unable to create statement proxy for slow query report.");
+            return statement;
+        }
+    }
+
+    private Object invokeProxy(Method method, Object[] args, Object statement, long time) throws Exception {
+        Object result = null;
+        String name = method.getName();
+        String sql = null;
+        Constructor<?> constructor = null;
+
+        if (this.compare("prepareStatement", name)) {
+            sql = (String) args[0];
+            constructor = this.getConstructor(1, PreparedStatement.class);
+            if (sql != null) {
+                this.prepareStatement(sql, time);
+            }
+        } else if (!this.compare("prepareCall", name)) {
             return statement;
         }
 
+        result = constructor.newInstance(new StatementProxy(statement, sql));
+        return result;
     }
 
     protected class StatementProxy implements InvocationHandler {
@@ -100,66 +104,86 @@ public class TimeLogInterceptor extends AbstractQueryReport {
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             String name = method.getName();
             boolean close = TimeLogInterceptor.this.compare("close", name);
-            if (close && this.closed) {
-                return null;
-            } else if (TimeLogInterceptor.this.compare("isClosed", name)) {
-                return this.closed;
-            } else if (this.closed) {
-                throw new SQLException("Statement closed.");
-            } else {
-                boolean process = false;
-                process = TimeLogInterceptor.this.isExecute(method, process);
-                long start = process ? System.currentTimeMillis() : 0L;
-                Object result = null;
+            try {
+                if (close && this.closed) {
+                    return null;
+                } else if (TimeLogInterceptor.this.compare("isClosed", name)) {
+                    return this.closed;
+                } else if (this.closed) {
+                    throw new SQLException("Statement closed.");
+                } else {
+                    boolean process = false;
+                    process = TimeLogInterceptor.this.isExecute(method, process);
+                    long start = process ? System.currentTimeMillis() : 0L;
+                    Object result = null;
 
-                try {
-                    result = method.invoke(this.delegate, args);
-                } catch (Throwable var13) {
-                    TimeLogInterceptor.this.reportFailedQuery(this.query, args, name, start, var13);
-                    if (var13 instanceof InvocationTargetException && var13.getCause() != null) {
-                        throw var13.getCause();
+                    result = this.delegate != null ? method.invoke(this.delegate, args) : null;
+
+
+                    long delta = process ? System.currentTimeMillis() - start : -9223372036854775808L;
+
+                    if (process) {
+                        TimeLogInterceptor.this.reportQuery(this.query, args, name, start, delta);
+                    }
+                    logQuery(start, delta);
+
+                    if (close) {
+                        this.closed = true;
+                        this.delegate = null;
                     }
 
-                    throw var13;
+                    return result;
                 }
-
-                long delta = process ? System.currentTimeMillis() - start : -9223372036854775808L;
-
-                if (process) {
-                    TimeLogInterceptor.this.reportQuery(this.query, args, name, start, delta);
-                }
-
-
-                logQuery(start, delta);
-                if (close) {
-                    this.closed = true;
-                    this.delegate = null;
-                }
-
-                return result;
+            } catch (Exception e) {
+                timeLog.error("Unable get query run-time");
+                return null;
             }
+
+
         }
 
         private void logQuery(long start, long delta) {
             try {
-                if (delegate instanceof PreparedStatement) {
+                if (this.delegate instanceof PreparedStatement) {
                     PreparedStatement preparedStatement = (PreparedStatement) this.delegate;
                     if (preparedStatement.getConnection() != null) {
                         DatabaseMetaData metaData = preparedStatement.getConnection().getMetaData();
                         if (timeLog.isDebugEnabled()) {
-                            Gson gson = new Gson();
-                            Map<String, String> log = new HashMap<>();
+                            Map<String, String> log = new LinkedHashMap<>();
+                            log.put("callType", "jdbc");
                             log.put("query", this.query);
                             log.put("startTime", Long.toString(start));
-                            log.put("delta", Long.toString(delta));
+                            log.put("delta", Long.toString(delta) + " ms");
                             log.put("connectionUrl", metaData.getURL());
-                            timeLog.debug(gson.toJson(log));
+                            timeLog.debug(toJson(log));
                         }
                     }
                 }
             } catch (Exception e) {
                 timeLog.error("Cannot get connection string ");
             }
+        }
+
+        private String toJson(Map<String, String> map) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("{");
+            Object[] keys = map.keySet().toArray();
+            for (int i = 0; i < keys.length; i++) {
+                sb.append("\"");
+                sb.append(keys[i]);
+                sb.append("\"");
+                sb.append(":");
+                sb.append("\"");
+                sb.append(map.get(keys[i]));
+                sb.append("\"");
+
+                if (i < keys.length - 1) {
+                    sb.append(",");
+                }
+            }
+            sb.append("}");
+
+            return sb.toString();
         }
     }
 }
